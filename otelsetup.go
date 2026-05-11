4dbe0c997351efdc7091e1db25c10abc0a616a36
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2026-05-11 15:34:29 +0900

    deps: upgrade otel modules to v1.43.0 and sync semconv to v1.40.0
    Signed-off-by: Kazumichi Yamamoto <yamamoto.febc@gmail.com>

--- otelsetup.go
+++ otelsetup.go
@@ -26,7 +26,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 )
 
 type Options struct {
